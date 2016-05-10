a9402b9f4b6fcbf67bf32838f7adf9d59f40fe3d
LeJacko <J.lemaitre@hotmail.fr>
2016-05-10 12:45:59 +0200

    travail sur les view

--- app/src/main/java/com/example/ensai/gloin/Enregistrement.java
+++ app/src/main/java/com/example/ensai/gloin/Enregistrement.java
@@ -2,7 +2,10 @@ package com.example.ensai.gloin;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by ensai on 06/05/16.
@@ -18,11 +21,29 @@ public class Enregistrement extends AppCompatActivity {
 
     }
     public void validerEnregistrement (View v){
-        Intent intent = new Intent (this ,MainActivity.class) ;
-        startActivity(intent) ;
+        boolean motDePasseCorrect;
+        boolean rempli ;
+        EditText motdepasse = (EditText) findViewById(R.id.motdepasse);
+        EditText motdepasse2 = (EditText) findViewById(R.id.motdepasse2);
+        EditText pseudo = (EditText) findViewById(R.id.pseudo);
+        EditText mail = (EditText) findViewById(R.id.mail);
+        EditText numero = (EditText) findViewById(R.id.numero);
+        rempli = (motdepasse.getText() != null && pseudo.getText().toString() != null && mail != null && numero != null);
+
+        motDePasseCorrect = ((String)motdepasse.getText().toString()).equals(motdepasse.getText().toString());
+        Toast.makeText(this,""+rempli+ " "+ motDePasseCorrect, Toast.LENGTH_LONG).show();
+
+        if (motDePasseCorrect && rempli) {
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        else Toast.makeText(this ,"erreur sur les mots de passe ou champs non rempli",Toast.LENGTH_LONG).show();
 
 
-    int toto = 0;
+
+
+
 
 }
 }
--- app/src/main/java/com/example/ensai/gloin/MainActivity.java
+++ app/src/main/java/com/example/ensai/gloin/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent) ;
     }
 
+
+
     //Commentaire inutile
 
 }
