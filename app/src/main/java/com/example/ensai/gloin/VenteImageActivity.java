6ea9e05080f0bb60dbeac3a5c3f84615e1973501
lomicmenes <lomicmenes@gmail.com>
2016-05-10 12:44:43 +0200

    Création de l'activité de vente d'une image

---
+++ app/src/main/java/com/example/ensai/gloin/VenteImageActivity.java
@@ -0,0 +1,16 @@
+package com.example.ensai.gloin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ensai on 10/05/16.
+ */
+public class VenteImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vente_image);
+    }
+}
