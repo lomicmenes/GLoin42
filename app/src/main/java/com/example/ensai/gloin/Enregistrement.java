8f49be0af10c62e33b9a72f941d812114d1599c3
lomicmenes <lomicmenes@gmail.com>
2016-05-10 10:30:50 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ensai/gloin/Enregistrement.java
@@ -0,0 +1,26 @@
+package com.example.ensai.gloin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by ensai on 06/05/16.
+ */
+public class Enregistrement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enregistrement);
+
+
+
+    }
+    public void validerEnregistrement (View v){
+        Intent intent = new Intent (this ,MainActivity.class) ;
+        startActivity(intent) ;
+
+}
+}
